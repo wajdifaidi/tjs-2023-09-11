15428ac1a53cc69f6979536341b9b0b9cbaebf7f
wajdi faidi <wajdi.faidi@gmail.com>
2023-09-11 11:50:59 +0100

    first component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+
+// autre façon de déclarer une function
+// function App(props){
+// }
+
+const App=(props) => {
+
+
+return    <div className='App'>Hello world</div>
+
+};
+
+export default App;
