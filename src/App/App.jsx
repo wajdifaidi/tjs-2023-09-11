b07c6f6f65b58e9f13b74f9c21d4e39643eba8e2
wajdi faidi <wajdi.faidi@gmail.com>
2023-09-11 16:40:00 +0100

    useEffect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,28 @@
-import react from 'react'
+import react, { useEffect, useState } from 'react'
 import Button from './components/Button/Button.jsx';
 
 // autre façon de déclarer une function
 // function App(props){
 // }
-
+//let counter =0;
 const App=(props) => {
+const [state,setState] = useState({counter:0,hello:"world"});
 
-
+useEffect( () =>{
+    console.log('dans le useEffect',state)
+},[state])
 return    <div className='App'>Hello world
+         valeur de counter : {state.counter}
             <hr/>
-            <Button text={1} bgColor ="tomato" />
-            <Button text={1} bgColor ="red"  onClick={(arg)=>{console.log('clické',arg)}}
-            />
+            <Button text={1} bgColor ="tomato" style={{width:"254px"}} onClick={()=>{
+                setState({...state,counter:state.counter+1});
+                console.log(state);
+            }}> +</Button>
+            <Button text={1} bgColor ="black" onClick={()=>{
+                setState({...state,counter:state.counter-1});
+                console.log(state);
+            }}>-</Button>
+          
          </div>
 
 };
--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import style from './Button.module.css'
 import PropTypes from 'prop-types'
+
 const Button = (props) => {
-  console.log(props);
+ // console.log(props);
     return (
         <button onClick={(evt)=>{
           props.onClick('ok');
-            console.log(evt);
+            //console.log(evt);
         }} className={style.Button} style={{
             backgroundColor:props.bgColor,
             ...props.style,
         
-        }}>{props.text}</button>
+        }}>{props.children}</button>
 
     )
 }
 
-Button.PropTypes={
-    text:PropTypes.string.isRequired,
+Button.propTypes={
+    children:PropTypes.any.isRequired,
     bgColor:PropTypes.oneOf(['tomato','black','transparent']).isRequired,
     onClick:PropTypes.func.isRequired,
     style: PropTypes.shape({
